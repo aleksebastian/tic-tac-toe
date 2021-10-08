9a75c96cefdecf8095bc28d02e7a8163d152c853
Alek <aleksebastiano@gmail.com>
2021-10-08 08:36:12 -0500

    Implemented game win and tie logic

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ for (var i = 0; i < boardSize; i++) {
   let tile = document.createElement("div");
   tile.classList.add("tile", "num" + tileCount.toString());
   board.appendChild(tile);
-
   tiles[tileCount] = tile;
   tileCount++;
 }
 
+// Game mechanics
 var x = true;
 
 let play = (tile, i) => {
@@ -44,45 +44,146 @@ let play = (tile, i) => {
       x = true;
     }
   }
-  let board = jsBoardRender(linearBoard);
-  checkGame(board);
+  let rows = jsRowRender(linearBoard);
+  let columns = jsColumnRender(rows);
+
+  checkGame(rows, columns);
 };
 
 // Tile click event handler
-Object.values(tiles).forEach((tile, i) => {
-  tile.onclick = function () {
-    play(tile, i);
-  };
-});
+let activateTiles = () => {
+  Object.values(tiles).forEach((tile, i) => {
+    tile.onclick = function () {
+      play(tile, i);
+    };
+  });
+};
+activateTiles();
 
-// JS board render
-let jsBoardRender = (linearBoard) => {
+// JS board render - Rows
+let jsRowRender = (linearBoard) => {
   let copy = linearBoard.slice();
-  let renderedBoard = [];
+  let renderedRows = [];
   while (copy.length) {
     let row = copy.splice(0, boardRows);
-    renderedBoard.push(row);
+    renderedRows.push(row);
   }
-  return renderedBoard;
+  return renderedRows;
 };
 
-// horizontal check
+// To refactor if time permits
+// JS board render - Columns
+let jsColumnRender = (linearBoard) => {
+  let renderedColumns = [];
+  let columnCount = 0;
+  let column1 = [];
+  let column2 = [];
+  let column3 = [];
+  for (var i = 0; i < linearBoard.length; i++) {
+    let currentRow = linearBoard[i];
+
+    for (var j = 0; j < currentRow.length; j++) {
+      let currentTile = currentRow[j];
+      if (j === 0) {
+        column1.push(currentTile);
+      }
+      if (j === 1) {
+        column2.push(currentTile);
+      }
+      if (j === 2) {
+        column3.push(currentTile);
+      }
+    }
+  }
+  renderedColumns.push(column1, column2, column3);
+
+  return renderedColumns;
+};
+
+// Horizontal and vertical condition
 const xIsWinner = (currentTile) => currentTile === "x";
 const oIsWinner = (currentTile) => currentTile === "o";
 
-// vertical check
+// tie check!!
 
-// diagonal check
+// Check for win or tie function
+let checkGame = (rowBoard, columnBoard) => {
+  let xMajorDiagonalCount = 0;
+  let xMinorDiagonalCount = 0;
+  let oMajorDiagonalCount = 0;
+  let oMinorDiagonalCount = 0;
+  let tieCheck = 0;
 
-// check for win or tie function
-let checkGame = (board) => {
-  for (var i = 0; i < board.length; i++) {
-    let currentRow = board[i];
-    if (currentRow.every(xIsWinner)) {
-      console.log("x wins!");
+  for (var i = 0; i < rowBoard.length; i++) {
+    let currentRow = rowBoard[i];
+    let currentColumn = columnBoard[i];
+    if (currentRow.every(xIsWinner) || currentColumn.every(xIsWinner)) {
+      winOrTieResponse("X");
     }
-    if (currentRow.every(oIsWinner)) {
-      console.log("o wins!");
+    if (currentRow.every(oIsWinner) || currentColumn.every(oIsWinner)) {
+      winOrTieResponse("O");
+    }
+    if (currentRow[i] === "x") {
+      xMajorDiagonalCount++;
+    }
+    if (currentRow[i] === "o") {
+      oMajorDiagonalCount++;
+    }
+    if (currentRow[currentRow.length - (i + 1)] === "x") {
+      xMinorDiagonalCount++;
+    }
+    if (currentRow[currentRow.length - (i + 1)] === "o") {
+      oMinorDiagonalCount++;
+    }
+    if (!currentRow.includes("null")) {
+      tieCheck++;
     }
   }
+  if (
+    xMajorDiagonalCount === rowBoard.length ||
+    xMinorDiagonalCount === rowBoard.length
+  ) {
+    winOrTieResponse("X");
+  } else if (
+    oMajorDiagonalCount === rowBoard.length ||
+    oMinorDiagonalCount === rowBoard.length
+  ) {
+    winOrTieResponse("O");
+  } else if (tieCheck === rowBoard.length) {
+    winOrTieResponse();
+  }
+};
+
+// Win or tie elements and functionality
+const winOrTieBanner = document.createElement("h1");
+const resetInfoElement = document.createElement("h3");
+const resetInfo = (resetInfoElement.innerHTML =
+  "Click the reset button below to play again");
+const HTMLBoard = document.querySelector(".board");
+
+let winOrTieResponse = (winner) => {
+  if (!winner) {
+    winOrTieBanner.innerHTML = "Cat's Game!";
+    winOrTieBanner.style.float = "left";
+    HTMLBoard.appendChild(winOrTieBanner);
+  } else {
+    winOrTieBanner.innerHTML = `${winner} wins!`;
+    HTMLBoard.prepend(winOrTieBanner);
+    Object.values(tiles).forEach((tile) => {
+      tile.onclick = null;
+    });
+  }
+};
+
+// Reset button
+let resetButton = document.querySelector(".reset");
+resetButton.onclick = () => {
+  Object.values(tiles).forEach((tile) => {
+    tile.innerHTML = "";
+  });
+  winOrTieBanner.innerHTML = "";
+  emptyBoard = Array(boardSize);
+  linearBoard = emptyBoard.fill("null");
+  activateTiles();
+  x = true;
 };
